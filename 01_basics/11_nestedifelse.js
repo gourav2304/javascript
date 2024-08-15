530377c85b3d4786961fc36516a18f22823db8ef
Gourav Barnwal <78587654+gourav2304@users.noreply.github.com>
2024-08-15 09:40:06 +0530

    onebygetting clear get commit

---
+++ 01_basics/11_nestedifelse.js
@@ -0,0 +1,40 @@
+//nested if else
+//winning no is 19;
+//if we guess 18
+//to low 
+//if we guess 20;
+//to high
+
+
+// let winningNumber = 19;
+// let guesserNumber = +prompt("guess a number")
+
+// if(userGuess ===winningNumber ){
+//     console.log("user guess right number");
+// }else{
+//     if(userGuess < winningNumber){
+//         console.log("you guess too low ");
+//     }else{
+//         console.log("you guess to high");
+//     }    
+// }
+
+//if 
+//else if
+//else if
+//else if
+//else if
+ let temperature = 20;
+ if(temperature>40){
+    console.log("temperature is to high");
+ }
+ else if(temperature>30){
+    console.log("lets go for swim");
+ }
+ else if(temperature>20){
+    console.log("its cold outside");
+ }
+ else if(temperature>10){
+    console.log("its cold not get outside home");
+ }
+ console.log("Hello")
---
+++ 01_basics/12_switchstat.js
@@ -0,0 +1,28 @@
+let day = 2;
+switch(day){
+    case 0:
+        console.log("sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case  3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("Thrusday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    case 7:
+        console.log("invalid");
+        break;
+    
+}
