95e0c67aa118ae243fc9dae657089f6a44bdfba3
Gourav Barnwal <78587654+gourav2304@users.noreply.github.com>
2024-08-29 23:26:50 +0530

    init commit

---
+++ 08_dom/02_file.js
@@ -0,0 +1,26 @@
+// this keyword
+
+// const btn = document.querySelector(".btn-headline");
+// btn.addEventListener("click",function() {
+//     console.log("you clicked me ones");
+//     console.log("this value is ");
+//     console.log(this);
+// });
+
+//Key press event
+
+// const body = document.body;
+// body.addEventListener("keypress",(e)=>{
+//     console.log(e.key)
+// })
+//mouseover event
+//mouseleave event
+const mainButton = document.querySelector(".btn-headline")
+mainButton.addEventListener("mouseover",()=>{
+    console.log("mouseover event occured")
+})
+
+mainButton.addEventListener("mouseleave",()=>{
+    console.log("mouseleave event occured")
+})
+
