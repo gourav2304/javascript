b5bacd9c1122be6b58d813124e66cb8685a6941c
Gourav Barnwal <78587654+gourav2304@users.noreply.github.com>
2024-09-01 23:21:51 +0530

    commit

--- colorchange/little_demo.js
+++ colorchange/little_demo.js
@@ -1,7 +1,7 @@
 const mainButton = document.querySelector("button")
 const body = document.body;
 const currentColor = document.querySelector(".current-color");
-console.log(currentColor);
+
 function randomColorGenerator(){
     const red = Math.floor(Math.random()* 256);
     const green = Math.floor(Math.random()* 256);
