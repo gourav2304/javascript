e4248d1bd20c26c0a21ce8f396df9b6072f5fadb
Gourav Barnwal <78587654+gourav2304@users.noreply.github.com>
2024-09-08 23:29:47 +0530

    update

---
+++ Math_object/math_ceil.js
@@ -0,0 +1,15 @@
+//Math.ceil()
+
+Math.ceil();
+// Round up any decimal number to nearest interger
+// var round = Math.ceil(0.00001)
+// var round = Math.ceil(0.5)
+// var round = Math.ceil(0.99)
+// var round = Math.ceil(1.01)
+// var round = Math.ceil(1.05)
+// var round = Math.ceil(1.5)
+// var round = Math.ceil(1.99)
+// var round = Math.ceil(2.05)
+// var round = Math.ceil(2.5)
+// var round = Math.ceil(2.99)
+console.log(round);
---
+++ Math_object/maths_random.js
@@ -0,0 +1,10 @@
+// Math.random()
+
+// Generate a decimal number lie between 0 to 0.99
+Math.random()
+ //save it to in creating variable
+var decimal = Math.random();
+// log the value of decimal to the console
+console.log(decimal);
+// log the value of decimal MULTIPLIED by 10 
+console.log(10 * decimal);
