1e049eb0ef5fc1c8a32803204951a80b8a66dff2
Gourav Barnwal <78587654+gourav2304@users.noreply.github.com>
2024-09-17 23:49:35 +0530

    update commit

---
+++ 08_dom/domfile.js
@@ -0,0 +1,31 @@
+// Task 2: Query the h1 element
+let h1 = document.querySelector('h1');
+
+// Task 3: Declare an array
+let arr = [
+    'Example Domain',
+    'First Click',
+    'Second Click',
+    'Third Click'
+];
+
+// Task 4: Write a click-handling function
+function handleClicks() {
+    // Switch statement based on h1.innerText
+    switch (h1.innerText) {
+        case arr[0]:
+            h1.innerText = arr[1];
+            break;
+        case arr[1]:
+            h1.innerText = arr[2];
+            break;
+        case arr[2]:
+            h1.innerText = arr[3];
+            break;
+        default:
+            h1.innerText = arr[0];
+    }
+}
+
+// Task 5: Add an event listener to the h1 element
+h1.addEventListener('click', handleClicks);
